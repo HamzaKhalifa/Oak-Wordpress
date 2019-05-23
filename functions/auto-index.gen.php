d96036ee8e63fd13a3cfff3876f2d57bfeba69cd
Hamza Khalifa <hamza.khalifa@esprit.tn>
2019-05-23 12:22:15 +0200

    Removed a few conditions

--- functions/auto-index.gen.php
+++ functions/auto-index.gen.php
@@ -30,10 +30,6 @@ endif;
 
     $all_posts_and_pages = array_merge( $posts, $pages );
 
-    wp_send_json_success( array(
-        'posts_and_pages' => $all_posts_and_pages
-    ) );
-
     foreach( $all_posts_and_pages as $post ) :
         $post_selected_objects = get_post_meta( $post->ID, 'objects_selector' ) ? get_post_meta( $post->ID, 'objects_selector' ) [0] : [];
 
