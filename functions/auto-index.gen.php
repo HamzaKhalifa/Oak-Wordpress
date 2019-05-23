68eb850aea8ac3ad738e1238f761ee0d6b2bbb0e
Hamza Khalifa <hamza.khalifa@esprit.tn>
2019-05-23 12:16:07 +0200

    Removed a few conditions

--- functions/auto-index.gen.php
+++ functions/auto-index.gen.php
@@ -5,10 +5,29 @@
     $posts = get_posts( array(
         'numberposts' => -1,
     ) );
-    wp_send_json_success( array(
-        'posts' => $posts
+
+    $pages = get_pages( array(
+        'sort_order' => 'asc',
+        'sort_column' => 'post_title',
+        'hierarchical' => 1,
+        'exclude' => '',
+        'include' => '',
+        'meta_key' => '',
+        'meta_value' => '',
+        'authors' => '',
+        'child_of' => 0,
+        'parent' => -1,
+        'exclude_tree' => '',
+        'number' => '',
+        'offset' => 0,
+        'post_type' => 'page',
+        'post_status' => 'publish'
     ) );
-    $pages = get_pages();
+
+
+    wp_send_json_success( array(
+        'pages' => $pages
+    ) );
 
     $all_posts_and_pages = array_merge( $posts, $pages );
 
