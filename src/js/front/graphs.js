df97f15a18d8cbaf895dd2d544c1b2900727608e
Hamza Khalifa <hamza.khalifa@esprit.tn>
2019-05-28 01:02:03 +0200

    Data studio worked

--- src/js/front/graphs.js
+++ src/js/front/graphs.js
@@ -4,9 +4,12 @@ handleGraphsInitialization();
 function handleGraphsInitialization() {
     var allGraphContainers = document.querySelectorAll('.oak_front_graph_container');
     console.log(allGraphContainers);
+
     for (var i = 0; i < allGraphContainers.length; i++) {
         console.log(allGraphContainers[i]);
-        var chartCreator = allGraphContainers[i].getContext('2d');
+        var canvas = document.createElement('canvas');
+        allGraphContainers[i].append(canvas);
+        var chartCreator = canvas.getContext('2d');
         var data = JSON.parse(allGraphContainers[i].getAttribute('data'));
 
         var chart = new Chart(chartCreator, data);
